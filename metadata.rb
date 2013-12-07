7295c7470d502eb3e2d4f1b77771e14fe13453f3
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-12-07 19:42:51 +0900

    fix issue

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,4 @@ description      "Installs/Configures phpunit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
 depends          "build-essential"
+depends          "php"
